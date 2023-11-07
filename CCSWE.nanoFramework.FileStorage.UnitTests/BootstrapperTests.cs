6dbadbea113e0defc57c2b8a5b6714e111ff574a
Cory Charlton <ccharlton@pobox.com>
2023-11-07 01:36:29 -0800

    Initial version

---
+++ CCSWE.nanoFramework.FileStorage.UnitTests/BootstrapperTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using nanoFramework.TestFramework;
+
+namespace CCSWE.nanoFramework.FileStorage.UnitTests
+{
+    [TestClass]
+    public class BootstrapperTests
+    {
+        [TestMethod]
+        public void AddFileStorage_should_register_FileStorage()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+
+            // Act
+            serviceCollection.AddFileStorage();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var result1 = serviceProvider.GetService(typeof(IFileStorage));
+            var result2 = serviceProvider.GetService(typeof(IFileStorage));
+
+            // Assert
+            Assert.IsNotNull(result1);
+            Assert.IsInstanceOfType(result1, typeof(FileStorage));
+            Assert.AreEqual(result1, result2);
+        }
+    }
+}
---
+++ CCSWE.nanoFramework.FileStorage/IFileStorage.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+
+namespace CCSWE.nanoFramework.FileStorage
+{
+    /// <summary>
+    /// Provides access to a file system.
+    /// </summary>
+    public interface IFileStorage
+    {
+        /// <summary>
+        /// Determines whether the specified file exists.
+        /// </summary>
+        /// <param name="path">The file to check.</param>
+        /// <returns><c>true</c> if the file exists; otherwise <c>false</c>.</returns>
+        bool FileExists(string path);
+
+        /// <summary>
+        /// Returns the names of subdirectories (including their paths) in the specified directory.
+        /// </summary>
+        /// <param name="path">The absolute path to the directory to search.</param>
+        string[] GetDirectories(string path);
+
+        /// <summary>
+        /// Returns the names of files (including their paths) in the specified directory.
+        /// </summary>
+        /// <param name="path">The absolute path to the directory to search.</param>
+        string[] GetFiles(string path);
+
+        /// <summary>
+        /// Opens an existing UTF-8 encoded text file for reading.
+        /// </summary>
+        /// <param name="path">The file to be opened for reading.</param>
+        /// <returns>A <see cref="StreamReader"/> on the specified path.</returns>
+        StreamReader OpenText(string path);
+        
+        /// <summary>
+        /// Opens a text file, reads all the text in the file, and then closes the file.
+        /// </summary>
+        /// <param name="path">The file to open for reading.</param>
+        string ReadAllText(string path);
+
+        /// <summary>
+        /// Creates a new file, writes the specified string to the file, and then closes the file. If the target file already exists, it is overwritten.
+        /// </summary>
+        /// <param name="path">The file to write to.</param>
+        /// <param name="contents">The string to write to the file.</param>
+        void WriteAllText(string path, string? contents);
+    }
+}
